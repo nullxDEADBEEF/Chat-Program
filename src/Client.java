fd166c82f60459451df0ca9a6a65d3d802bb6fa3
nullxDEADBEEF <andxdgu@gmail.com>
2020-02-25 13:43:31 +0100

    Added serversocket on the server and socket for the client

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,54 @@
-public class Client {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+// NOTE: Client must send a "hearbeat alive" message every minute
+// NOTE: Client must send a Quit message when it is closing
+
+public class Client implements Runnable {
+    private static InetAddress host;
+    private static final int PORT =  1337;
+    private static String username;
+
+    public void run() {
+
+    }
 
     public static void main( String[] args ) {
+        System.out.print( "Enter your desired chat-name: " );
+        Scanner inputScanner = new Scanner( System.in );
+        username = inputScanner.nextLine();
 
+        try {
+            host = InetAddress.getLocalHost();
+        } catch ( UnknownHostException e ) {
+            System.out.println( "Unable to find ID of host!" );
+            e.printStackTrace();
+            System.exit( 1 );
+        }
+
+        connectToServer();
+    }
+
+    private static void connectToServer() {
+        Socket connection = null;
+
+        try {
+            connection = new Socket( host, PORT );
+        } catch ( IOException e ) {
+            System.out.println( "Could not connect!" );
+            e.printStackTrace();
+            System.exit( 1 );
+        } finally {
+            try {
+                System.out.println( "*** CLOSING CONNECTION... ***" );
+                connection.close();
+            } catch ( IOException e ) {
+                System.out.println( "Unable to disconnect!" );
+                System.exit( 1 );
+            }
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,65 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+// NOTE: Must use threads
+// NOTE: Must accept more than 1 client to join the chat system
+
 public class Server {
+    private static ServerSocket serverSocket;
+    private static final int PORT = 1337;
+    private static final int MAX_THREADS = 5;
+    private static final int MAX_CLIENTS = 5;
+    private static ThreadPoolExecutor threadPoolExecutor;
+    private static ArrayList<String> activeUsers;
 
     public static void main( String[] args ) {
+        System.out.println( "Opening port: " + PORT );
 
+        threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool( MAX_THREADS );
+        activeUsers = new ArrayList<>();
+
+        try {
+            serverSocket = new ServerSocket( PORT );
+        } catch ( IOException e ) {
+            System.out.println( "Unable to attach port!" );
+            e.printStackTrace();
+            System.exit( 1 );
+        }
+
+        while ( !serverSocket.isClosed() ) {
+            serveClient();
+        }
+
+        threadPoolExecutor.shutdown();
+    }
+
+    /*
+    handle messages from the client
+     */
+    private static void serveClient() {
+        // create a socket to establish connection
+        Socket clientConnection = null;
+
+        // listen for a connection to be made to the server socket
+        try {
+            clientConnection = serverSocket.accept();
+
+
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        } finally {
+            try {
+                System.out.println( "Closing..." );
+                clientConnection.close();
+            } catch ( IOException e ) {
+                System.out.println( "Could not disconnect!" );
+                e.printStackTrace();
+                System.exit( 1 );
+            }
+        }
     }
 }
